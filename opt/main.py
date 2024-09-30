15a67195a1b8d4526adb4c946004d68012d9771a
sauhits <atsushi.obata1207@gmail.com>
2024-09-30 18:31:23 +0900

    [add] SumPrice #5

--- opt/main.py
+++ opt/main.py
@@ -23,13 +23,13 @@ def recordData(db:list):
         if price_tmp=="re":
             continue
         printLine()
-        class_tmp=input("CLASS\n")
-        if class_tmp=="re":
+        type_tmp=input("TYPE\n")
+        if type_tmp=="re":
             continue
         printLine()
-        print("\nDATE: "+date_tmp+"\nNAME: "+store_tmp+"\nPRICE: "+price_tmp+"\nCLASS: "+class_tmp+"\n")
+        print("\nDATE: "+date_tmp+"\nNAME: "+store_tmp+"\nPRICE: "+price_tmp+"\nTYPE: "+type_tmp+"\n")
         if input("\nOK? Y/n\n")=="Y"or"y":
-            insertDataToDB(db,[date_tmp,store_tmp,price_tmp,class_tmp])
+            insertDataToDB(db,[date_tmp,store_tmp,price_tmp,type_tmp])
             print("complete!\n")
         else:
             print("cancel\n")
@@ -44,25 +44,44 @@ def insertDataToDB(db:list,data:list):
 
 #DBの表示
 def showDB(db:list):
-    print(tab(db,headers=['date','name','price','class'],tablefmt='github',numalign='left'))
+    print(tab(db,headers=['date','name','price','type'],tablefmt='github',numalign='left'))
 
 #データの表示
 def showData(data:list):
-    print(tab(data,headers=['date','name','price','class'],tablefmt='github',numalign='left'))
+    print(tab(data,headers=['date','name','price','type'],tablefmt='github',numalign='left'))
 
 #lineの表示
 def printLine():
     print("---------------------------------------------------")
+
+# home表示
+def printHome():
+    # 予算の算出をする
+    SumExpenditureOfLoan=SumDBPrice(ExpenditureDB,"loan")
+    SumIncomeOfLoan=SumDBPrice(IncomeDB,"loan")
+    SumExpenditureOfFree=SumDBPrice(ExpenditureDB,"free")
+    SumIncomeOfFree=SumDBPrice(IncomeDB,"free")
+    BalanceOfLoan=SumIncomeOfLoan-SumExpenditureOfLoan
+    BalanceOfFree=SumIncomeOfFree-SumExpenditureOfFree
+    
+    printLine()
+    print("HELLO!\n\nBalance Loan:"+str(BalanceOfLoan)+"\n        Free:"+str(BalanceOfFree)+"\n\nplease select option\n| INCOME | EXPENDITURE |budget| SHOW |delete| QUIT |")
+    printLine()
+
+# データの金額合算の作成
+def SumDBPrice(db:list,type:str):
+    SUM_tmp=0;int
+    for data_price in db:
+        if type==data_price[3]:
+            SUM_tmp+=int(data_price[2])
+    return SUM_tmp
 
 def main():
     # 支出データの読み込み
     ExpenditureDB:list=preReaderDB("Expenditure.csv")
     IncomeDB:list=preReaderDB("Income.csv")
     while True:
-        printLine()
-        print("HELLO!\nplease select option\n| INCOME | EXPENDITURE |budget| SHOW |delete| QUIT |")
-        printLine()
-        
+        printHome()
         option:str=input().lower()
         
         #選択肢フィルター            
